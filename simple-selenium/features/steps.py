8d437d0b85d3dd7327a290510d7cc22846613c1e
zbo <zbo2@qq.com>
2015-10-20 14:36:11 +0800

    add lettuce and selenium sample

---
+++ simple-selenium/features/steps.py
@@ -0,0 +1,33 @@
+__author__ = 'bob.zhu'
+
+from lettuce import *
+from selenium import webdriver
+import time
+import pdb
+
+
+@step("open page \'([^\']*)\'")
+def open_page(step, url):
+    # pdb.set_trace()
+    world.browser = webdriver.Chrome()
+    world.browser.get(url)
+
+@step("When I click element with class \'([^\']*)\'")
+def when_i_click_element_with_class(step,css):
+    # pdb.set_trace()
+    world.first_tab = world.browser.find_element_by_class_name(css)
+    world.first_tab.click()
+    time.sleep(2)
+
+@step("When I click element with text \'([^\']*)\'")
+def when_i_click_element_with_text(step,text):
+    link = world.first_tab.find_elements_by_link_text("Mi Note")[0]
+    link.click()
+
+@step("Then I see the \'([^\']*)\' in the title")
+def then_i_see_the_group1_in_the_title(step, text):
+    assert text in world.browser.title
+    world.browser.close()
+
+
+
