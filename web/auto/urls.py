95f853c35df9dfc099b25fa430e093d18d8cc32e
zbo2@qq.com <zbo2@qq.com>
2015-10-21 14:38:09 +0800

    add first view and route

---
+++ web/auto/urls.py
@@ -0,0 +1,15 @@
+__author__ = 'bob.zhu'
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # ex: /auto/
+    url(r'^$', views.index, name='index'),
+    # ex: /auto/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /auto/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /auto/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
