6dfa34c8d0b819f2d14c14c9ed1ca9ff5d5052b3
zbo2@qq.com <zbo2@qq.com>
2015-10-23 10:30:10 +0800

    fix get step tests import parent folder from sys path

--- tests/test_get_step.py
+++ tests/test_get_step.py
@@ -1,16 +1,17 @@
-from steploader import load_steps
-
 __author__ = 'bob.zhu'
+
+import sys
 import lettuce
 
+sys.path.append('../')
+from steploader import load_steps
 
-base_path='../simple-selenium'
-tags=None
+base_path = '../simple-selenium'
+tags = None
 l_runner = lettuce.Runner(
-        base_path=base_path
-    )
+    base_path=base_path
+)
 modules = load_steps(base_path)
 
 for i in range(len(modules)):
     print modules[i].STEP_REGISTRY
-
