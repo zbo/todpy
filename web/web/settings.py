a02b80d70aa30a9fc41203d74c1c0cdf5f5b479e
ian.zhang <ed.zhang1988@gmail.com>
2015-10-29 16:03:12 +0800

    add react for scenario editing

--- web/web/settings.py
+++ web/web/settings.py
@@ -10,8 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from os.path import abspath, basename, dirname, join, normpath
 
+BASE_DIR = dirname(dirname(abspath(__file__)))
+SITE_ROOT = dirname(BASE_DIR)  
+SITE_NAME = basename(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -39,6 +42,8 @@ INSTALLED_APPS = (
     'auto',
     'config',
     'django_extensions',
+    'pipeline',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -83,5 +88,65 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 STATICFILES_DIRS = ('./web/static',)
+
+# Configure templates
+# TEMPLATES = [  
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': ['./web/templates'],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
 TEMPLATE_DIRS = ('./web/templates',)
+
+# Django Pipeline (and browserify)
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+STATICFILES_FINDERS = (  
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'pipeline.finders.PipelineFinder',
+)
+
+# browserify-specific
+PIPELINE_COMPILERS = (  
+    'pipeline_browserify.compiler.BrowserifyCompiler',
+)
+
+PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.NoopCompressor'  
+PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
+
+if DEBUG:  
+    PIPELINE_BROWSERIFY_ARGUMENTS = '-t babelify'
+
+PIPELINE_CSS = {  
+    'mysite_css': {
+        'source_filenames': (
+            'css/style.css',
+        ),
+        'output_filename': 'css/mysite_css.css',
+    },
+}
+
+PIPELINE_JS = {  
+    'mysite_js': {
+        'source_filenames': (
+            # 'js/bower_components/jquery/dist/jquery.min.js',
+            # 'js/bower_components/react/JSXTransformer.js',
+            # 'js/bower_components/react/react-with-addons.js',
+            # 'js/app.browserify.js',
+        ),
+        # 'output_filename': 'js/mysite_js.js',
+    }
+}
