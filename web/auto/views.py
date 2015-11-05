99844a5ff6bd95c50a3b4ab56789bea7e3528c0c
zbo <zbo2@qq.com>
2015-11-06 00:19:50 +0800

    add get feature by id service

--- web/auto/views.py
+++ web/auto/views.py
@@ -4,7 +4,7 @@ sys.path.append('../../')
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from models import Feature, Scenario, Step
-from dto import DataEncoder, StepDto
+from dto import DataEncoder, StepDto, FeatureDto, ScenarioDto
 from django.views.decorators.csrf import csrf_exempt
 from auto.saver import StepDtoPostSaver
 import json
@@ -34,15 +34,15 @@ def save_feature(request):
         result = saver.save(json_data)
         return HttpResponse(request.body)
 
-def sample(request):
-    return render(request, 'auto/sample.html')
+def get_feature(request, feature_id):
+    feature = Feature.objects.filter(id=feature_id).first()
+    feature_dto = FeatureDto(feature.name, feature.description)
+    scenarios = []
+    for sce in feature.scenario_set.all():
+        s_dto = ScenarioDto(sce.description)
+        s_dto.fill_steps(sce.step_set.all())
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+        scenarios.append(s_dto)
+    feature_dto.fill_scenarios(scenarios)
+    return HttpResponse(json.dumps((feature_dto),cls=DataEncoder), content_type="application/json")
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
