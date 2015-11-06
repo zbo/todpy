a8f037f5e132738fb59fd3581a85897b9d4b34f4
zbo <zbo2@qq.com>
2015-11-06 15:00:56 +0800

    update feature cover with test

---
+++ tests/test_update_all_by_feature.py
@@ -0,0 +1,135 @@
+__author__ = 'bob.zhu'
+import sys
+import os
+import django
+import json
+import uuid
+import unittest
+
+sys.path.append('../web')
+sys.path.append('../../')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+# Shell Plus Model Imports
+from auto.models import Feature, Scenario, Step
+from auto.dto import StepDto
+from auto.saver import StepDtoPostSaver, StepDtoPostUpdater
+from config.models import AppSetting, FeatureLocation
+from workspace.models import WorkSpace
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+# Shell Plus Django Imports
+from django.utils import timezone
+from django.conf import settings
+from django.core.cache import cache
+from django.db.models import Avg, Count, F, Max, Min, Sum, Q, Prefetch
+from django.core.urlresolvers import reverse
+from django.db import transaction
+from django.contrib import admin
+
+def test():
+    django.setup()
+    all_steps = '''{
+            "feature": {
+                "feature_name": "change name feature name",
+                "feature_description": "not fun now",
+                "feature_id": 1,
+                "scenarios": [
+                    {
+                        "scenario_name": "first updated scenario",
+                        "scenario_id": 1,
+                        "steps": [
+                            {
+                                "1": {
+                                    "co_firstlineno": 8,
+                                    "co_name": "i_open_browser",
+                                    "step_name": "I open web browser",
+                                    "co_argcount": 1,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_browser.py",
+                                    "co_varnames": [
+                                        "step"
+                                    ]
+                                }
+                            },
+                            {
+                                "new": {
+                                    "co_firstlineno": 13,
+                                    "co_name": "i_open_page",
+                                    "step_name": "I open page '([^']*)'",
+                                    "co_argcount": 2,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_browser.py",
+                                    "co_varnames": [
+                                        "step",
+                                        "url"
+                                    ]
+                                }
+                            },
+                            {
+                                "3": {
+                                    "co_firstlineno": 14,
+                                    "co_name": "i_click_element_with_text",
+                                    "step_name": "I click element with text '([^']*)'",
+                                    "co_argcount": 2,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_click.py",
+                                    "co_varnames": [
+                                        "step",
+                                        "text",
+                                        "element"
+                                    ]
+                                }
+                            },
+                            {
+                                "4": {
+                                    "co_firstlineno": 18,
+                                    "co_name": "i_close_browser",
+                                    "step_name": "I close web browser",
+                                    "co_argcount": 1,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_browser.py",
+                                    "co_varnames": [
+                                        "step"
+                                    ]
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "scenario_name": "second new created scenario",
+                        "scenario_id": "new",
+                        "steps": [
+                            {
+                                "new": {
+                                    "co_firstlineno": 8,
+                                    "co_name": "i_open_browser",
+                                    "step_name": "I open web browser",
+                                    "co_argcount": 1,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_browser.py",
+                                    "co_varnames": [
+                                        "step"
+                                    ]
+                                }
+                            },
+                            {
+                                "new": {
+                                    "co_firstlineno": 13,
+                                    "co_name": "i_open_page",
+                                    "step_name": "I open page '([^']*)'",
+                                    "co_argcount": 2,
+                                    "co_file_name": "/Users/bob.zhu/project/todpy/libraries/web/action/features/web_browser.py",
+                                    "co_varnames": [
+                                        "step",
+                                        "url"
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        }'''
+
+    updater = StepDtoPostUpdater()
+    result = updater.update(all_steps)
+    return result
+if __name__ == '__main__':
+    test()
--- web/auto/saver.py
+++ web/auto/saver.py
@@ -1,9 +1,10 @@
 __author__ = 'bob.zhu'
 import json
 from models import Feature, Scenario, Step
+import pdb
 
 
-class StepDtoPostSaver:
+class CommonSaver:
     def __init__(self):
         pass
 
@@ -13,6 +14,58 @@ class StepDtoPostSaver:
             tmp += s + ','
         return tmp[0:len(tmp) - 1] + ']'
 
+    def update_sequence(self, scenario, sequence):
+        scenario.set_sequence(sequence)
+        scenario.save()
+
+    def save_new_step(self, sce_save, step):
+        step = step[step.keys()[0]]
+        step_save = Step().fill(sce_save, step['step_name'], step['co_name'], step['co_file_name'],
+                                step['co_firstlineno'], step['co_argcount'],
+                                self.replace_u(step['co_varnames']))
+        step_save.save()
+        return step_save
+
+    def save_new_scenario(self, feature_save, sce):
+        sequence = ''
+        sce_save = Scenario().fill(feature_save, sce['scenario_name'], '')
+        sce_save.save()
+        steps = sce['steps']
+        for step in steps:
+            step_save = self.save_new_step(sce_save, step)
+            sequence += str(step_save.id) + '|'
+        self.update_sequence(sce_save, sequence)
+
+    def update_existing_scenario(self, feature_update, sce_update, sce_exist):
+        sequence = ''
+        sce_exist.fill(feature_update, sce_update['scenario_name'], '')
+        sce_exist.save()
+        steps = sce_update['steps']
+        for step in steps:
+            step_id = step.keys()[0]
+
+            if str(step_id).lower() == 'new':
+                step_saved = self.save_new_step(sce_exist, step)
+                sequence += str(step_saved.id) + '|'
+            else:
+                self.update_exist_step(sce_exist, step)
+                sequence += str(step_id) + '|'
+        self.update_sequence(sce_exist, sequence)
+
+    def update_exist_step(self, sce_exist, step):
+        step_id = step.keys()[0]
+        step = step[step.keys()[0]]
+        step_exist = Step.objects.get(pk=step_id)
+        step_exist.fill(sce_exist, step['step_name'], step['co_name'], step['co_file_name'],
+                        step['co_firstlineno'], step['co_argcount'],
+                        self.replace_u(step['co_varnames']))
+        step_exist.save()
+
+
+class StepDtoPostSaver:
+    def __init__(self):
+        self.CommonSaver = CommonSaver()
+
     def save(self, json_str):
         json_obj = json.loads(json_str)
         feature_name = json_obj['feature']['feature_name']
@@ -20,19 +73,29 @@ class StepDtoPostSaver:
         feature_save = Feature().fill(feature_name, feature_desc, '', '')
         feature_save.save()
         scenarios = json_obj['feature']['scenarios']
-
         for sce in scenarios:
-            sequence = ''
-            sce_save = Scenario().fill(feature_save, sce['scenario_name'], '')
-            sce_save.save()
-            steps = sce['steps']
-            for step in steps:
-                step = step[step.keys()[0]]
-                step_save = Step().fill(sce_save, step['step_name'], step['co_name'], step['co_file_name'],
-                                        step['co_firstlineno'], step['co_argcount'],
-                                        self.replace_u(step['co_varnames']))
-                step_save.save()
-                sequence += str(step_save.id) + '|'
-            sce_save.set_sequence(sequence)
-            sce_save.save()
+            self.CommonSaver.save_new_scenario(feature_save, sce)
         return feature_save
+
+
+class StepDtoPostUpdater:
+    def __init__(self):
+        self.CommonSaver = CommonSaver()
+
+    def update(self, json_str):
+        json_obj = json.loads(json_str)
+        feature_name = json_obj['feature']['feature_name']
+        feature_desc = json_obj['feature']['feature_description']
+        feature_id = json_obj['feature']['feature_id']
+        feature_update = Feature.objects.get(id=feature_id)
+        feature_update.fill(feature_name, feature_desc, '', '')
+        feature_update.save()
+        scenarios = json_obj['feature']['scenarios']
+        for sce in scenarios:
+            scenario_id = sce['scenario_id']
+            if str(scenario_id).lower() == 'new':
+                self.CommonSaver.save_new_scenario(feature_update, sce)
+            else:
+                scenario_exist = Scenario.objects.get(id=scenario_id)
+                self.CommonSaver.update_existing_scenario(feature_update, sce, scenario_exist)
+        return feature_update
