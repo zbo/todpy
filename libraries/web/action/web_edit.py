41f1cde43beee86034b8ac82d95b7d83df800f9c
zbo <zbo2@qq.com>
2015-11-09 15:27:24 +0800

    fix tests after merge

--- libraries/web/action/web_edit.py
+++ libraries/web/action/web_edit.py
@@ -4,10 +4,9 @@ from selenium import webdriver
 import time
 import pdb
 
-
 @step("I input \'([^\']*)\' into textbox with id \'([^\']*)\'")
 def i_type_text(step, text, id):
-
+    element = world.browser.find_element_by_id(id)
     element.click()
     element.send_keys(text)
 
