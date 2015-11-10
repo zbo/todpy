ee48f9536b4a3beb4f1056b33748a919f39fc494
zbo <zbo2@qq.com>
2015-11-10 14:26:31 +0800

    fix feature description tests

--- tests/unittests/assert_feature_file.py
+++ tests/unittests/assert_feature_file.py
@@ -3,7 +3,7 @@ from auto.models import Feature, Scenario, Step
 import json
 
 def assert_gen_file(instance, plain_text):
-    instance.assertEqual(plain_text[0],'Feature: not fun now')
-    instance.assertEqual(plain_text[3],"    When I open page 'http://www.okcoin.com/'")
-    instance.assertEqual(plain_text[6],"  Scenario: second new created scenario")
+    instance.assertEqual(plain_text[1],'  not fun now')
+    instance.assertEqual(plain_text[4],"    When I open page 'http://www.okcoin.com/'")
+    instance.assertEqual(plain_text[7],"  Scenario: second new created scenario")
     pass
--- web/auto/generator.py
+++ web/auto/generator.py
@@ -9,8 +9,10 @@ class FeatureFileGenerator:
     def generate_feature(feature):
         plain_text = []
         feature_description= feature.description
-        feature_file_name = "{0}-{1}".format(str(feature.id),feature_description.replace(' ','-'))
-        plain_text.append("Feature: {0}".format(feature_description))
+        feature_name = feature.name
+
+        plain_text.append("Feature: {0}".format(feature_name))
+        plain_text.append("  {0}".format(feature_description))
         for scenario in feature.scenario_set.filter(deleted=0):
             plain_text.append("  Scenario: {0}".format(scenario.description))
             sequence = scenario.step_sequence
@@ -25,8 +27,8 @@ class FeatureFileGenerator:
     def save_feature_file(feature, workspace):
         plain_text = FeatureFileGenerator.generate_feature(feature)
         base_location = workspace.getFolderPath()
-
-        file_path = "{0}/{1}/features/{2}.feature".format(base_location,workspace.type,feature.name.replace(' ','-'))
+        feature_file_name = "{0}-{1}".format(str(feature.id),feature.name.replace(' ','-'))
+        file_path = "{0}/{1}/features/{2}.feature".format(base_location,workspace.type,feature_file_name)
         file_object = open(file_path, 'a')
         for line in plain_text:
             file_object.write(line+'\r\n')
