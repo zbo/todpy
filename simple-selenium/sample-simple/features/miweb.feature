84e687726950b29cea803bbd5472c1019c18c0b4
zbo <zbo2@qq.com>
2015-11-13 14:14:00 +0800

    move samples to single folder

--- simple-selenium/sample-simple/features/miweb.feature
+++
@@ -1,8 +0,0 @@
-Feature: mi note device info page
-  mi note is one kind of xiao mi device, make sure info page alive
-
-   Scenario: should open the mi note page
-    Given I open page 'http://www.mi.com/en/'
-    When I click element with class 'nav-item'
-     When I click element with text 'Mi Note'
-     Then I see the 'Note' in the title
--- worker/task_monitor.py
+++ worker/task_monitor.py
@@ -1,22 +1,36 @@
 __author__ = 'bob.zhu'
+import os
+import sys
+sys.path.append('../web/web/')
+sys.path.append('../web/')
+sys.path.append('../../')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from datetime import datetime
 import time
 from plan_retriver import *
-import thread
+import django
 
 def work():
-  print "hello world."
-  iter_now = datetime.now()
-  iter_now_time = iter_now.strftime('%Y-%m-%d %H:%M:%S')
-  print iter_now_time
-  single_plan = sqlite_execution_plan_retriver.retrive_first_one()
+    single_plan = sqlite_execution_plan_retriver.retrive_first_one()
+    if single_plan is None:
+        heart_beat()
+    else:
+        sqlite_execution_plan_retriver.mark_execution_as_running(single_plan)
+        print 'work execution found id:{0}, booked time:{1}, ==== start!'.format(single_plan.id, single_plan.starttime)
 
+
+def heart_beat():
+    iter_now = datetime.now()
+    iter_now_time = iter_now.strftime('%Y-%m-%d %H:%M:%S')
+    print iter_now_time
+    print 'heart beat work plan cleaned ----- :-)'
 
 
 def runTask(func, rest_time):
-  while True:
-    time.sleep(rest_time)
-    func()
-    continue
+    django.setup()
+    while True:
+        time.sleep(rest_time)
+        func()
+        continue
 
 runTask(work, rest_time=2)
